730897d77e15121b36630ab335c953773d55e185
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2024-05-23 07:20:12 -0400

    removed color string crate, writing my own

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,39 @@
 //! Personal LS command implementation
-use colored::Colorize;
 use core::cmp::Ordering;
 use core::fmt::Error;
 use rust_ls::argument_parsing::Arguments;
-use std::env;
+use std::env::args;
 use std::fs::{read_dir, ReadDir};
 
-/// Sorts two strings.
-fn alphabetic_order_with_uppercase_first(
-    left: &String,
-    right: &String,
-) -> Ordering {
-    let Some(first_left_char) = left.chars().next() else {
-        println!("Left filename is empty!");
+/// Holds the RGB values of some color.
+struct Color {
+    /// The RGB to output
+    rgb: [u8; 3],
+}
+
+/// A string that supports color printing out of the box.
+struct ColoredString {
+    /// The text to output
+    input: String,
+    /// The RGB color to output
+    rgb: Color,
+}
+
+impl ColoredString {}
+
+/// Sorts two strings with lowercase letters first.
+fn alphabetic_lowercase_first(left: &String, right: &String) -> Ordering {
+    let Some(left_first_char) = left.chars().next() else {
         return Ordering::Less;
     };
 
-    let Some(first_right_char) = right.chars().next() else {
-        println!("Right filename is empty!");
+    let Some(right_first_char) = right.chars().next() else {
         return Ordering::Less;
     };
 
     match (
-        first_left_char.is_uppercase(),
-        first_right_char.is_uppercase(),
+        left_first_char.is_lowercase(),
+        right_first_char.is_lowercase(),
     ) {
         (true, false) => Ordering::Less,
         (false, true) => Ordering::Greater,
@@ -41,35 +51,24 @@ fn get_paths(paths: ReadDir) -> Result<Vec<String>, Error> {
             return Err(Error);
         };
 
-        let Ok(mut name) = path.file_name().into_string() else {
+        let Ok(name) = path.file_name().into_string() else {
             eprintln!("Failed to convert {path:?} into a string.");
             return Err(Error);
         };
-
-        if name.starts_with('.') {
-            continue;
-        }
 
         let Ok(file_type) = path.file_type() else {
             eprintln!("Failed to get file type for {path:?}.");
             return Err(Error);
         };
 
-        if file_type.is_dir() {
-            name = name.blue().bold().to_string();
-        }
-
         result.push(name);
     }
-
-    // TODO: folders show up before files but should be opposite
-    result.sort_by(alphabetic_order_with_uppercase_first);
 
     Ok(result)
 }
 
 fn main() {
-    let all_raw_args = env::args().collect::<Vec<String>>();
+    let all_raw_args = args().collect::<Vec<String>>();
     let raw_arg_array = all_raw_args.get(1..);
     let argument_parsing_outcome = raw_arg_array.map_or_else(
         || Arguments::new(&vec![]),
@@ -89,12 +88,18 @@ fn main() {
         return;
     };
 
-    let Ok(filenames) = get_paths(paths) else {
+    let Ok(mut filenames) = get_paths(paths) else {
         eprintln!("Failed to get all paths in the directory.");
         return;
     };
 
-    let output = filenames.join("  ");
+    filenames.sort_by(alphabetic_lowercase_first);
 
-    println!("  {output}");
+    let filtered_filenames = filenames
+        .into_iter()
+        .filter(|filename| !filename.starts_with('.'))
+        .collect::<Vec<String>>();
+
+    let output = filtered_filenames.join("  ");
+    println!("{output}");
 }
