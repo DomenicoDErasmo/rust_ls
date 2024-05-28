f1b942f3430ce5b3d622b56c3b6bd074ad0bb657
Domenico D'Erasmo <domenicoderasmo99@gmail.com>
2024-05-28 07:16:00 -0400

    starting to implement glob_with

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 //! Personal LS command implementation
+use glob::{glob_with, MatchOptions};
 use rust_ls::argument_parsing::setup_args;
 use rust_ls::output::Output;
 use std::fs::{read_dir, DirEntry};
@@ -12,23 +13,43 @@ fn main() {
         }
     };
 
-    let Ok(directory) = read_dir(args.path.clone()) else {
-        eprintln!("Failed to read directory {}.", args.path);
+    let options = MatchOptions {
+        case_sensitive: false,
+        ..Default::default()
+    };
+    let Ok(globs) = glob_with(&args.path, options) else {
+        eprintln!("Failed to glob {}.", &args.path);
         return;
     };
+    for glob in globs {
+        let Ok(path) = glob else {
+            eprintln!("Failed to get path for {glob:#?}.");
+            return;
+        };
 
-    let Ok(dir_entries): Result<Vec<DirEntry>, _> =
-        directory.into_iter().collect()
-    else {
-        eprintln!("Failed to collect paths");
-        return;
-    };
+        // TODO: 
+        // read_dir on the target directory for the regex pattern
+        // use its canonical path
+        // if we have a file that matches the pattern, add to output
+        // if we have a directory that matches the pattern, read_dir it
+        let Ok(directory) = read_dir(path.clone()) else {
+            eprintln!("Failed to read directory {path:#?}.");
+            return;
+        };
 
-    let Some(paths) = Output::new(dir_entries) else {
-        eprintln!("Failed to create OutputEntry from dir_entries.");
-        return;
-    };
+        let Ok(dir_entries): Result<Vec<DirEntry>, _> =
+            directory.into_iter().collect()
+        else {
+            eprintln!("Failed to collect paths");
+            return;
+        };
 
-    let output = paths.output(&args);
-    println!("{output}");
+        let Some(paths) = Output::new(dir_entries) else {
+            eprintln!("Failed to create OutputEntry from dir_entries.");
+            return;
+        };
+
+        let output = paths.output(&args);
+        println!("{output}");
+    }
 }
